57176e3f2f013738867301c19c03303824e8a076
uEngine-Solutions <help@uengine.org>
2022-10-17 11:50:48 +0000

    first commit

---
+++ src/main/java/com/eventstorming/infra/PolicyHandler.java
@@ -0,0 +1,101 @@
+representativeFor: Policy
+path: {{name}}/{{{options.packagePath}}}/infra
+mergeType: template
+---
+package {{options.package}}.infra;
+
+import javax.naming.NameParser;
+
+import javax.naming.NameParser;
+import javax.transaction.Transactional;
+
+import {{options.package}}.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import {{options.package}}.domain.*;
+
+
+@Service
+@Transactional
+@Configuration
+public class PolicyHandler{
+    {{#aggregates}}
+    @Autowired {{namePascalCase}}Repository {{nameCamelCase}}Repository;
+    {{/aggregates}}
+    
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whatever(@Payload String eventString){}
+
+    {{#policies}}
+    {{#relationAggregateInfo}}
+    @Autowired
+    {{../../options.package}}.external.{{aggregateValue.namePascalCase}}Service {{aggregateValue.nameCamelCase}}Service;
+
+    {{/relationAggregateInfo}}
+
+
+    @Bean
+    public DomainEventDispatcher domainEventDispatcher(DomainEventDispatcherFactory domainEventDispatcherFactory) {
+      return domainEventDispatcherFactory.make("orderServiceEvents", DomainEventHandlersBuilder
+      .forAggregateType("{{labshopeventuate.domain.Order}}")
+      .onEvent(OrderPlaced.class, PolicyHandler::wheneverOrderPlaced_DecreaseStock)
+      //.onEvent(OrderCancelledEvent.class, this::handleOrderCancelledEvent)
+      .build());
+    }
+
+
+    {{log relationEventInfo}}
+    
+    {{#relationEventInfo}}
+    @StreamListener(value=KafkaProcessor.INPUT, condition="headers['type']=='{{eventValue.namePascalCase}}'")
+    public void whenever{{eventValue.namePascalCase}}_{{../namePascalCase}}(@Payload {{eventValue.namePascalCase}} {{eventValue.nameCamelCase}}){
+
+        {{eventValue.namePascalCase}} event = {{eventValue.nameCamelCase}};
+        System.out.println("\n\n##### listener {{../namePascalCase}} : " + {{eventValue.nameCamelCase}} + "\n\n");
+
+        {{#../relationAggregateInfo}}
+        // REST Request Sample
+        
+        // {{aggregateValue.nameCamelCase}}Service.get{{aggregateValue.namePascalCase}}(/** mapping value needed */);
+
+        {{/../relationAggregateInfo}}
+
+        {{#todo ../description}}{{/todo}}
+
+        // Sample Logic //
+        {{#../aggregateList}}
+        {{namePascalCase}}.{{../../nameCamelCase}}(event);
+        
+        {{/../aggregateList}}
+
+        
+
+    }
+        {{/relationEventInfo}}
+
+    {{/policies}}
+}
+
+
+
+<function>
+window.$HandleBars.registerHelper('todo', function (description) {
+
+    if(description){
+        description = description.replaceAll('\n','\n\t\t// ')
+        return description = '// Comments // \n\t\t//' + description;
+    }
+     return null;
+});
+
+window.$HandleBars.registerHelper('log', function (msg) {
+
+    console.log(msg);
+     return null;
+});
+
+</function>
